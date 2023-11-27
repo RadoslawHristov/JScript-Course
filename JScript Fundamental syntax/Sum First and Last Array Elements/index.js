4bb67ffd61c8272fe403e33ceef10e763535dc88
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2023-11-27 18:30:48 +0200

    Add files via upload

---
+++ JScript Fundamental syntax/Sum First and Last Array Elements/index.js
@@ -0,0 +1,12 @@
+function solve(input) {
+   let arr = [];
+
+
+    let firstElement = Number(input[0]);
+    let lastElement = Number(input[input.length-1]);
+
+    console.log(Number(firstElement + lastElement))
+}
+
+
+solve([20 ,10, 40])
