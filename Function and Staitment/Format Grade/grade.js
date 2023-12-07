6e3a45edcbe516c6d64071505e17c08a9203f9e6
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2023-12-07 14:47:50 +0200

    Add files via upload

---
+++ Function and Staitment/Format Grade/grade.js
@@ -0,0 +1,27 @@
+function gradeResult(grade) {
+    
+    if (grade < 3.00) {
+        console.log('Fail (2)');
+    } else if (grade < 3.50){
+        console.log(`Poor (${grade.toFixed(2)})`)
+    } else{
+        grades(grade);
+    }
+
+
+
+    function grades(grade) {
+        
+        if (grade > 3.50 && grade < 4.50) {
+            console.log(`Good (${grade.toFixed(2)})`);
+        } if(grade >= 4.50 && grade < 5.50){
+            console.log(`Very good (${grade.toFixed(2)})`);
+        } if(grade >= 5.50){
+            console.log(`Excellent (${grade.toFixed(2)})`);
+        }
+    }
+}
+
+gradeResult(3.33);
+
+gradeResult(4.50);
