64ebe796ff9c20f44a1820ec969972d074c1e785
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2023-12-08 16:16:56 +0200

    Add files via upload

---
+++ Function and Staitment/Leap Year/year.js
@@ -0,0 +1,11 @@
+function divisibleYear (year) {
+    
+    if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+        console.log('yes')
+    }else{
+        console.log('no')
+    }
+}
+
+
+divisibleYear(2003)
