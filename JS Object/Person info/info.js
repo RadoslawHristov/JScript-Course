e19cb62b76bced173cf14a1044d008221adfa6ee
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2024-01-02 10:47:13 +0200

    Add files via upload

---
+++ JS Object/Person info/info.js
@@ -0,0 +1,13 @@
+function personInfo (firstName,lastName, age) {
+    
+    let info = {};
+    info.firstName = firstName;
+    info.lastName = lastName;
+    info.age = age;
+
+   return info;
+}
+
+personInfo("Peter", 
+"Pan",
+"20");
