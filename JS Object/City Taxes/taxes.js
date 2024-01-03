3484b4bce25dce91b82f9a826f27213901a7a37d
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2024-01-03 14:34:42 +0200

    Add files via upload

---
+++ JS Object/City Taxes/taxes.js
@@ -0,0 +1,48 @@
+function createRecord(name, population, treasury) {
+    return {
+        name, population, treasury,
+        taxRate: 10,
+        collectTaxes() {
+            this.treasury += this.population * this.taxRate;
+        },
+        applyGrowth(percent) {
+            this.population += Math.floor(this.population * percent / 100);
+        },
+          applyRecession(percent) {
+            this.treasury -= Math.floor(this.treasury * percent / 100);
+          },
+        };
+    }  
+
+const city = 
+    createRecord('Tortuga', 7000,15000)
+    city.collectTaxes()
+    console.log(city.treasury)
+    city.applyGrowth(5)
+  console.log(city.population);
+
+function replay(name,population,treasury){
+    return {
+        name,population,treasury,
+        taxRate: 10,
+        collectTaxes(){
+            treasury += population * this.taxRate;
+        },
+        applyGrowth(percent){
+            population += Math.floor(population * percent / 100);
+        },
+        applyRecession(percent){
+            treasury -= Math.floor(treasury * percent / 100);
+        }
+    }
+}
+
+const recs = 
+    replay('Tortuga', 7000,15000)
+    recs.collectTaxes()
+    console.log(recs.treasury)
+    recs.applyGrowth(5)
+    console.log(recs.population);
+    recs.applyRecession(10);
+    console.log(recs.treasury)
+
