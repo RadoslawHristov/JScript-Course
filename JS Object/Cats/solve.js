9a696a44e7ff51f17628be391b7a9050155f7db5
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2024-01-06 18:21:02 +0200

    Add files via upload

---
+++ JS Object/Cats/solve.js
@@ -0,0 +1,27 @@
+function classCat(input) {
+    
+    class Cat{
+        constructor(name,age){
+            this.name = name;
+            this.age = age;
+        }
+
+        meow(){
+            console.log(`${this.name}, age ${this.age} says Meow`);
+        }
+    }
+
+    let cats = [];
+
+    for (const line of input) {
+        let [name,age] = line.split(' ');
+        cats.push(new Cat(name,Number(age)));
+    }
+
+    for (const cat of cats) {
+        cat.meow();
+    }
+}
+
+
+classCat(['Mellow 2', 'Tom 5']);
