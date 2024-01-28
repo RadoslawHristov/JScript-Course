cfc9c61438286c096cfdef22b5820793b0266b4d
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2024-01-28 17:44:39 +0200

    Add files via upload

---
+++ JS Dom/Show-More/ShowMore.js
@@ -0,0 +1,9 @@
+function showText() {
+    
+    let getText = document.getElementById("text");
+    let getOIpt = document.getElementById("more");
+
+    getOIpt.style.display = 'none';
+    getText.style.display = 'inline';
+
+}
